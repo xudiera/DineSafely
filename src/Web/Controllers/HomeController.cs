ca71f97fcb636f77607d212055ad2b306d525610
Carlos Xudiera <xudiera@gmail.com>
2024-03-27 19:07:53 +0000

    build: Enforcing code style on build

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -4,28 +4,22 @@ using Web.Models;
 
 namespace Web.Controllers;
 
-public class HomeController : Controller
+public partial class HomeController(ILogger<HomeController> logger) : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<HomeController> _logger = logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    public IActionResult Index() => View();
 
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    public IActionResult Privacy() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        var requestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        LogRequestIdOnError(requestId);
+        return View(new ErrorViewModel { RequestId = requestId });
     }
+
+    [LoggerMessage(0, LogLevel.Information, "Error on request with id: {requestId}")]
+    partial void LogRequestIdOnError(string requestId);
 }
