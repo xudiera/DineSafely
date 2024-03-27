ce24e8e1dc0caa3edd668a216ac9e20c7846b7b2
Carlos Xudiera <xudiera@gmail.com>
2024-03-27 20:33:15 +0000

    feat: Removed Privacy view

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -10,8 +10,6 @@ public partial class HomeController(ILogger<HomeController> logger) : Controller
 
     public IActionResult Index() => View();
 
-    public IActionResult Privacy() => View();
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
