eb0680c328987b02d3f19ea4efed0acdeca4df37
Carlos Xudiera <xudiera@gmail.com>
2024-03-28 04:30:19 +0000

    feat: Added MiniProfiler

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -11,12 +11,18 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 
+builder.Services.AddMiniProfiler().AddEntityFramework();
+
 builder.Services.AddRepositories();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMiniProfiler();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
