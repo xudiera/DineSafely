1e48a486d14b1b4189b3a20a339216ceecd8fb34
Carlos Xudiera <xudiera@gmail.com>
2024-03-28 14:32:25 +0000

    feat: Added show in map

--- src/Web/ViewModels/InspectionDetailViewModel.cs
+++ src/Web/ViewModels/InspectionDetailViewModel.cs
@@ -6,5 +6,15 @@ namespace Web.ViewModels;
 public class InspectionDetailViewModel
 {
     public required int Id { get; set; }
+
+    public string MapUrl
+    {
+        get
+        {
+            var establishment = InspectionDetails.First().Inspection.Establishment;
+            return $"https://www.google.com/maps/search/?api=1&query={establishment.Name}%2C{establishment.Address}%2CToronto%2CON";
+        }
+    }
+
     public required PaginatedList<InspectionDetail> InspectionDetails { get; set; }
 }
