096f08dcb8c84d6e8557e6ec944c234c4d325663
Carlos Xudiera <xudiera@gmail.com>
2024-04-02 21:14:27 +0000

    build: Setup HTTPS certificate

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# HTTPS certificate
+dotnet dev-certs https
+sudo -E dotnet dev-certs https -ep /usr/local/share/ca-certificates/aspnet/https.crt --format PEM
+sudo update-ca-certificates
+
 # Restore dotnet tools
 dotnet tool restore
 
